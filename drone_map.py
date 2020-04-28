3481d24e3d9995fe118ae812591b94dd7028080e
Brendon Dulam <brendon.dulam@veoneer.com>
2020-04-28 22:40:43 +0900

    parse lidar points

--- drone_map.py
+++ drone_map.py
@@ -1,4 +1,5 @@
 import csv
+import math
 
 import pandas as pd
 
@@ -9,6 +10,7 @@ class DroneMap:
         self.flight_path_csv = flight_path_csv
 
         self.drone_positions = None
+        self.points = None
 
     def get_drone_positions(self) -> pd.DataFrame:
         if self.drone_positions is None:
@@ -27,6 +29,40 @@ class DroneMap:
             )
         return self.drone_positions
 
+    def get_all_points(self, max_sweep=-1) -> (pd.DataFrame, pd.DataFrame):
+        pos = self.get_drone_positions()
+        if self.points is None:
+            x, y = [], []
+
+            with open(file=self.lidar_points_csv) as f:
+                i = next_row_header = 0
+                pos_x, pos_y = 0, 0
+                for cf_row in csv.DictReader(f, fieldnames=['angle', 'distance']):
+                    if i == next_row_header:
+                        cf_sweep = int(float(cf_row['angle']))
+
+                        if cf_sweep > max_sweep > 0:
+                            break
+                        pos_x, pos_y = pos.iloc[cf_sweep].x, pos.iloc[cf_sweep].y
+                        next_row_header = i + int(cf_row['distance']) + 1
+                    else:
+                        theta, r = cf_row['angle'], cf_row['distance']
+                        theta = math.radians(float(theta))
+                        curr_x = float(r) * math.cos(theta) + pos_x
+                        curr_y = float(r) * math.sin(theta) + pos_y
+                        x.append(curr_x)
+                        y.append(curr_y)
+                    i += 1
+
+            self.points = pd.DataFrame(
+                data=list(zip(x, y)),
+                columns=['x', 'y']
+            )
+            self.points = self.points.drop_duplicates(keep='last')
+            self.points = self.points.sort_values(by=['x', 'y'])
+
+        return self.points
+
 
 if __name__ == '__main__':
     dm = DroneMap(
@@ -34,4 +70,5 @@ if __name__ == '__main__':
         flight_path_csv='./.cache/FlightPath.csv'
     )
 
-    print(dm.get_drone_positions())
+    # print(dm.get_drone_positions())
+    print(dm.get_all_points())
