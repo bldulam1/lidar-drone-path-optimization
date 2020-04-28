947d40774f090c4fa21096c927a90907b312b502
Brendon Dulam <brendon.dulam@veoneer.com>
2020-04-28 22:33:31 +0900

    test csv files

--- drone_map.py
+++ drone_map.py
@@ -2,3 +2,7 @@ class DroneMap:
     def __init__(self, lidar_points_csv: str, flight_path_csv: str):
         self.lidar_points_csv = lidar_points_csv
         self.flight_path_csv = flight_path_csv
+
+
+if __name__ == '__main__':
+    dm = DroneMap()
---
+++ lidar_point.py
@@ -0,0 +1,22 @@
+class LidarPoint:
+    def __init__(self, x, y):
+        self.x, self.y = x, y
+        self.neighbors = set()
+
+        self.opening_pair = None
+
+    def add_neighbor(self, neighbor):
+        self.neighbors.add(neighbor)
+
+    def __repr__(self):
+        p = (int(self.x), int(self.y))
+        return str(p)
+
+    def __str__(self):
+        return self.__repr__()
+
+    def __eq__(self, other):
+        return self.__repr__() == other.__repr__()
+
+    def __hash__(self):
+        return hash(self.__repr__())
