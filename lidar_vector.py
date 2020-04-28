5b22ae3ed0a5278dc2162fa7a356e378a65313c4
Brendon Dulam <brendon.dulam@veoneer.com>
2020-04-29 01:05:48 +0900

    get intersection point with

--- lidar_vector.py
+++ lidar_vector.py
@@ -13,6 +13,30 @@ class LidarVector:
         self.distance = math.sqrt(dx * dx + dy * dy)
         self.direction = math.atan2(dy, dx)
 
+    def get_intersection_point_with(self, point: LidarPoint, angle: float) -> LidarPoint:
+        """
+        Calculates the intersection between two this line and the line formed by a point and angle
+        :param point: LidarPoint
+        :param angle: float in radians
+        :return: LidarPoint
+        """
+        point2 = LidarPoint(x=point.x + math.cos(angle), y=point.y + math.sin(angle))
+        vector = LidarVector(point, point2)
+
+        a1, b1, c1 = vector.get_abc()
+        a2, b2, c2 = self.get_abc()
+
+        if a1 == 0 and a2 == 0:
+            return None
+        elif b1 == 0 and b2 == 0:
+            return None
+        else:
+            a = np.array([[a1, b1], [a2, b2]])
+            b = np.array([c1, c2])
+            intersection = np.linalg.solve(a, b)
+
+        return LidarPoint(x=intersection[0], y=intersection[1])
+
     def __repr__(self):
         return "{} {:.4f}∠{:.2f}".format(self.start, self.distance, math.degrees(self.direction))
 
