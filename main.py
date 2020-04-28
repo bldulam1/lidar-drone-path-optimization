415fc1c50faba766c9331d16e843cac36fdaf55a
Brendon Dulam <brendon.dulam@veoneer.com>
2020-04-29 00:06:44 +0900

    visualize by scan id in main

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 # TODO: Display
+from drone_map import DroneMap
+
+
+def visualize(drone_map: DroneMap, by_scan_id:bool):
+    drone_map.visualize_lidar_points(by_scan_id=by_scan_id)
+
 
 if __name__ == '__main__':
-    print("Hello")
+    dm = DroneMap(
+        lidar_points_csv='./.cache/LIDARPoints.csv',
+        flight_path_csv='./.cache/FlightPath.csv'
+    )
+
+    # visualize(drone_map=dm, by_scan_id=False)
